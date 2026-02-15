bcee239757d8aee97178df5e27f15cfaa877960e
hannanpoonawala <hannanpoonawala29@gmail.com>
2026-02-15 07:38:49 +0000

    update the linker and startup code

--- startup_stm32f103c8t6.c
+++ startup_stm32f103c8t6.c
@@ -2,48 +2,133 @@
 
 #include <stdint.h>
 
-#define STACK_START 0x20005000  /* 20KB RAM */
-
-/* Function prototypes */
-void Reset_Handler(void);
-void Default_Handler(void);
-
 /* External symbols from linker script */
+extern uint32_t _estack;  // Defined in linker script
 extern uint32_t _sdata, _edata, _sidata;
 extern uint32_t _sbss, _ebss;
 extern int main(void);
+extern void sysclk_init(void);
 
-/* Weak aliases for exception handlers */
-void NMI_Handler(void) __attribute__((weak, alias("Default_Handler")));
-void HardFault_Handler(void) __attribute__((weak, alias("Default_Handler")));
-void MemManage_Handler(void) __attribute__((weak, alias("Default_Handler")));
-void BusFault_Handler(void) __attribute__((weak, alias("Default_Handler")));
-void UsageFault_Handler(void) __attribute__((weak, alias("Default_Handler")));
-void SVC_Handler(void) __attribute__((weak, alias("Default_Handler")));
-void DebugMon_Handler(void) __attribute__((weak, alias("Default_Handler")));
-void PendSV_Handler(void) __attribute__((weak, alias("Default_Handler")));
-/* SysTick_Handler is defined in systick.c, so don't alias it */
-void SysTick_Handler(void);
+/* Core exception handlers */
+void Reset_Handler(void);
+void Default_Handler(void);
+
+void NMI_Handler(void)              __attribute__((weak, alias("Default_Handler")));
+void HardFault_Handler(void)        __attribute__((weak, alias("Default_Handler")));
+void MemManage_Handler(void)        __attribute__((weak, alias("Default_Handler")));
+void BusFault_Handler(void)         __attribute__((weak, alias("Default_Handler")));
+void UsageFault_Handler(void)       __attribute__((weak, alias("Default_Handler")));
+void SVC_Handler(void)              __attribute__((weak, alias("Default_Handler")));
+void DebugMon_Handler(void)         __attribute__((weak, alias("Default_Handler")));
+void PendSV_Handler(void)           __attribute__((weak, alias("Default_Handler")));
+void SysTick_Handler(void)          __attribute__((weak, alias("Default_Handler")));
+
+/* Peripheral interrupt handlers (STM32F103 medium density) */
+void WWDG_IRQHandler(void)          __attribute__((weak, alias("Default_Handler")));
+void PVD_IRQHandler(void)           __attribute__((weak, alias("Default_Handler")));
+void TAMPER_IRQHandler(void)        __attribute__((weak, alias("Default_Handler")));
+void RTC_IRQHandler(void)           __attribute__((weak, alias("Default_Handler")));
+void FLASH_IRQHandler(void)         __attribute__((weak, alias("Default_Handler")));
+void RCC_IRQHandler(void)           __attribute__((weak, alias("Default_Handler")));
+void EXTI0_IRQHandler(void)         __attribute__((weak, alias("Default_Handler")));
+void EXTI1_IRQHandler(void)         __attribute__((weak, alias("Default_Handler")));
+void EXTI2_IRQHandler(void)         __attribute__((weak, alias("Default_Handler")));
+void EXTI3_IRQHandler(void)         __attribute__((weak, alias("Default_Handler")));
+void EXTI4_IRQHandler(void)         __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel1_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel2_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel3_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel4_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel5_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel6_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void DMA1_Channel7_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void ADC1_2_IRQHandler(void)        __attribute__((weak, alias("Default_Handler")));
+void USB_HP_CAN_TX_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void USB_LP_CAN_RX0_IRQHandler(void)__attribute__((weak, alias("Default_Handler")));
+void CAN_RX1_IRQHandler(void)       __attribute__((weak, alias("Default_Handler")));
+void CAN_SCE_IRQHandler(void)       __attribute__((weak, alias("Default_Handler")));
+void EXTI9_5_IRQHandler(void)       __attribute__((weak, alias("Default_Handler")));
+void TIM1_BRK_IRQHandler(void)      __attribute__((weak, alias("Default_Handler")));
+void TIM1_UP_IRQHandler(void)       __attribute__((weak, alias("Default_Handler")));
+void TIM1_TRG_COM_IRQHandler(void)  __attribute__((weak, alias("Default_Handler")));
+void TIM1_CC_IRQHandler(void)       __attribute__((weak, alias("Default_Handler")));
+void TIM2_IRQHandler(void)          __attribute__((weak, alias("Default_Handler")));
+void TIM3_IRQHandler(void)          __attribute__((weak, alias("Default_Handler")));
+void TIM4_IRQHandler(void)          __attribute__((weak, alias("Default_Handler")));
+void I2C1_EV_IRQHandler(void)       __attribute__((weak, alias("Default_Handler")));
+void I2C1_ER_IRQHandler(void)       __attribute__((weak, alias("Default_Handler")));
+void I2C2_EV_IRQHandler(void)       __attribute__((weak, alias("Default_Handler")));
+void I2C2_ER_IRQHandler(void)       __attribute__((weak, alias("Default_Handler")));
+void SPI1_IRQHandler(void)          __attribute__((weak, alias("Default_Handler")));
+void SPI2_IRQHandler(void)          __attribute__((weak, alias("Default_Handler")));
+void USART1_IRQHandler(void)        __attribute__((weak, alias("Default_Handler")));
+void USART2_IRQHandler(void)        __attribute__((weak, alias("Default_Handler")));
+void USART3_IRQHandler(void)        __attribute__((weak, alias("Default_Handler")));
+void EXTI15_10_IRQHandler(void)     __attribute__((weak, alias("Default_Handler")));
+void RTCAlarm_IRQHandler(void)      __attribute__((weak, alias("Default_Handler")));
+void USBWakeUp_IRQHandler(void)     __attribute__((weak, alias("Default_Handler")));
 
 /* Vector table */
-uint32_t vectors[] __attribute__((section(".isr_vector"))) = {
-    STACK_START,                    /* Initial stack pointer */
-    (uint32_t)Reset_Handler,        /* Reset Handler */
-    (uint32_t)NMI_Handler,          /* NMI Handler */
-    (uint32_t)HardFault_Handler,    /* Hard Fault Handler */
-    (uint32_t)MemManage_Handler,    /* MPU Fault Handler */
-    (uint32_t)BusFault_Handler,     /* Bus Fault Handler */
-    (uint32_t)UsageFault_Handler,   /* Usage Fault Handler */
-    0,                              /* Reserved */
-    0,                              /* Reserved */
-    0,                              /* Reserved */
-    0,                              /* Reserved */
-    (uint32_t)SVC_Handler,          /* SVCall Handler */
-    (uint32_t)DebugMon_Handler,     /* Debug Monitor Handler */
-    0,                              /* Reserved */
-    (uint32_t)PendSV_Handler,       /* PendSV Handler */
-    (uint32_t)SysTick_Handler,      /* SysTick Handler */
-    /* Add more interrupt handlers as needed */
+__attribute__((section(".isr_vector")))
+const void *vector_table[] = {
+    &_estack,  // Initial stack pointer
+    Reset_Handler,
+    NMI_Handler,
+    HardFault_Handler,
+    MemManage_Handler,
+    BusFault_Handler,
+    UsageFault_Handler,
+    0, 0, 0, 0,
+    SVC_Handler,
+    DebugMon_Handler,
+    0,
+    PendSV_Handler,
+    SysTick_Handler,
+
+    /* External Interrupts */
+    WWDG_IRQHandler,
+    PVD_IRQHandler,
+    TAMPER_IRQHandler,
+    RTC_IRQHandler,
+    FLASH_IRQHandler,
+    RCC_IRQHandler,
+    EXTI0_IRQHandler,
+    EXTI1_IRQHandler,
+    EXTI2_IRQHandler,
+    EXTI3_IRQHandler,
+    EXTI4_IRQHandler,
+    DMA1_Channel1_IRQHandler,
+    DMA1_Channel2_IRQHandler,
+    DMA1_Channel3_IRQHandler,
+    DMA1_Channel4_IRQHandler,
+    DMA1_Channel5_IRQHandler,
+    DMA1_Channel6_IRQHandler,
+    DMA1_Channel7_IRQHandler,
+    ADC1_2_IRQHandler,
+    USB_HP_CAN_TX_IRQHandler,
+    USB_LP_CAN_RX0_IRQHandler,
+    CAN_RX1_IRQHandler,
+    CAN_SCE_IRQHandler,
+    EXTI9_5_IRQHandler,
+    TIM1_BRK_IRQHandler,
+    TIM1_UP_IRQHandler,
+    TIM1_TRG_COM_IRQHandler,
+    TIM1_CC_IRQHandler,
+    TIM2_IRQHandler,
+    TIM3_IRQHandler,
+    TIM4_IRQHandler,
+    I2C1_EV_IRQHandler,
+    I2C1_ER_IRQHandler,
+    I2C2_EV_IRQHandler,
+    I2C2_ER_IRQHandler,
+    SPI1_IRQHandler,
+    SPI2_IRQHandler,
+    USART1_IRQHandler,
+    USART2_IRQHandler,
+    USART3_IRQHandler,
+    EXTI15_10_IRQHandler,
+    RTCAlarm_IRQHandler,
+    USBWakeUp_IRQHandler
 };
 
 void Reset_Handler(void)
@@ -62,6 +147,8 @@ void Reset_Handler(void)
     while (dst < (volatile uint8_t*)&_ebss) {
         *dst++ = 0;
     }
+
+    sysclk_init();  // Initialize system clock before main
 
     /* Call main */
     main();
